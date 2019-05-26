a6719b101c046aaf903ae02e2259372acb5cf7d4
ginpei <ginpei@gmail.com>
2019-05-26 16:35:21 -0700

    update to look circle clearer

--- assets/PomodoroCircle.js
+++ assets/PomodoroCircle.js
@@ -32,8 +32,9 @@ export default class PomodoroCircle {
     const rate = ps.workTime / (ps.breakTime + ps.workTime);
     const point = rate * 360;
 
-    this._renderCircle(el, 0, point, 'orange');
-    this._renderCircle(el, point, 360, 'green');
+    this._renderCircle(el, 0, point, 'orange', 'tomato');
+    this._renderCircle(el, point, 360, 'green', 'darkgreen');
+    this._renderCircle(el, 0, 0, 'orange', 'tomato');
   }
 
   /**
@@ -41,8 +42,9 @@ export default class PomodoroCircle {
    * @param {number} deg1
    * @param {number} deg2
    * @param {string} fillStyle
+   * @param {string} strokeStyle
    */
-  _renderCircle (elCanvas, deg1, deg2, fillStyle) {
+  _renderCircle (elCanvas, deg1, deg2, fillStyle, strokeStyle) {
     const ctx = elCanvas.getContext('2d');
     if (!ctx) {
       throw new Error('Failed to get canvas context');
@@ -51,9 +53,13 @@ export default class PomodoroCircle {
     const { height, width } = elCanvas;
     const cx = width / 2;
     const cy = height / 2;
-    const radius = Math.min(height, width) * 0.5;
+    const fullRadius = Math.min(height, width);
+    const radius = fullRadius * 0.49;
     const thickness = radius / 4;
 
+    ctx.fillStyle = fillStyle;
+    ctx.strokeStyle = strokeStyle;
+    ctx.lineWidth = fullRadius * 0.01;
     ctx.beginPath();
 
     // outer stroke
@@ -67,7 +73,7 @@ export default class PomodoroCircle {
     ctx.arc(cx, cy, radius - thickness, degToRad(deg2), degToRad(deg1), true);
 
     ctx.closePath();
-    ctx.fillStyle = fillStyle;
     ctx.fill();
+    ctx.stroke();
   }
 }
